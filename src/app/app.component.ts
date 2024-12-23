96569d8de86df516c8e82510920aca04f870797b
chetanmundlesd <chetanmundlesd@gmail.com>
2024-12-23 18:18:27 +0530

    change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,27 @@
-import { AsyncPipe, CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import {
+  AfterContentChecked,
+  ChangeDetectorRef,
+  Component,
+  inject,
+} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoaderService } from './core/services/index';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AsyncPipe, CommonModule],
+  imports: [RouterOutlet, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements AfterContentChecked {
   title = 'EHRApplicationFrontend';
 
   loadingService = inject(LoaderService);
+
+  constructor(private cdref: ChangeDetectorRef) {}
+  ngAfterContentChecked(): void {
+    this.cdref.detectChanges();
+  }
 }
